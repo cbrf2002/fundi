62dec2d00674ab762543b6d3bea3668f4064e295
cbrf2002 <cbrf2002@proton.me>
2024-12-30 00:43:10 +0800

    Early implementation of auth in splash

--- lib/features/splash/screens/splash_screen.dart
+++ lib/features/splash/screens/splash_screen.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
     Future.delayed(Duration(seconds: 2), () {
-      Navigator.pushReplacementNamed(context, '/main'); // Replace with auth logic.
+      FirebaseAuth.instance.authStateChanges().listen((User? user) {
+        if (user == null) {
+          Navigator.pushReplacementNamed(context, '/login');
+        } else {
+          Navigator.pushReplacementNamed(context, '/dashboard');
+        }
+      });
     });
 
     return Scaffold(
@@ -16,7 +23,7 @@ class SplashScreen extends StatelessWidget {
           mainAxisSize: MainAxisSize.max,
           children: [
             SvgPicture.asset(
-              'lib/assets/logo/fundi_logotext_base.svg',
+              'lib/assets/images/logo/logoDark.svg',
               width: 150,
               height: 150,
             ),
