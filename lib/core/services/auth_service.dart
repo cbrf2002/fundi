b6a362bcfe0aee9c8ad8be36ec9a0250148d42a1
cbrf2002 <cbrf2002@proton.me>
2024-12-30 01:15:04 +0800

    Temporarily commented out the google sign in functionality

--- lib/core/services/auth_service.dart
+++ lib/core/services/auth_service.dart
@@ -6,14 +6,16 @@ class AuthService {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   Future<User?> signInWithGoogle() async {
-    final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-    final GoogleSignInAuthentication googleAuth = await googleUser!.authentication;
-    final AuthCredential credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
-    UserCredential userCredential = await _auth.signInWithCredential(credential);
-    return userCredential.user;
+    // final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+    // final GoogleSignInAuthentication googleAuth = await googleUser!.authentication;
+    // final AuthCredential credential = GoogleAuthProvider.credential(
+    //   accessToken: googleAuth.accessToken,
+    //   idToken: googleAuth.idToken,
+    // );
+    // UserCredential userCredential = await _auth.signInWithCredential(credential);
+    // return userCredential.user;
+    print('IN DEVELOPMENT');
+    return null;
   }
 
   Future<void> signOut() async {
