96b07b867bf20136fd95001b92877eb89727509e
cbrf2002 <cbrf2002@proton.me>
2024-12-30 12:43:48 +0800

    fixed the forever loop on the splash screen

--- lib/features/splash/screens/splash_screen.dart
+++ lib/features/splash/screens/splash_screen.dart
@@ -1,40 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
-
-    Future.delayed(Duration(seconds: 2), () {
-      FirebaseAuth.instance.authStateChanges().listen((User? user) {
-        if (user == null) {
-          Navigator.pushReplacementNamed(context, '/login');
-        } else {
-          Navigator.pushReplacementNamed(context, '/dashboard');
-        }
-      });
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacementNamed(
+        context,
+        '/login', // Update navigation logic based on user state
+      );
     });
 
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
           children: [
-            SvgPicture.asset(
-              'lib/assets/images/logo/logoDark.svg',
-              width: 150,
-              height: 150,
-            ),
-            const Text(
+            SvgPicture.asset('lib/assets/logo/logoDark.svg', width: 150, height: 150),
+            const SizedBox(height: 16),
+            Text(
               'Fundi',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
+              style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
             ),
-          ]
+          ],
         ),
       ),
     );
