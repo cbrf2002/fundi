7a40111bdd01e0b9b15c8f469984d3b4e0585c6a
cbrf2002 <cbrf2002@proton.me>
2024-12-30 22:37:57 +0800

    Decluttered and improved ui

--- lib/widgets/custom_bottom_nav_bar.dart
+++ lib/widgets/custom_bottom_nav_bar.dart
@@ -1,37 +1,18 @@
 import 'package:flutter/material.dart';
 
-class CustomBottomNavBar extends StatefulWidget {
+class CustomBottomNavBar extends StatelessWidget {
   final int currentIndex;
   final ValueChanged<int> onTap;
 
   const CustomBottomNavBar({super.key, required this.currentIndex, required this.onTap});
 
   @override
-  _CustomBottomNavBarState createState() => _CustomBottomNavBarState();
-}
-
-class _CustomBottomNavBarState extends State<CustomBottomNavBar> with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(vsync: this, duration: Duration(milliseconds: 300));
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return BottomNavigationBar(
-      currentIndex: widget.currentIndex,
-      onTap: widget.onTap,
-      selectedItemColor: theme.colorScheme.onSurface,
+      currentIndex: currentIndex,
+      onTap: onTap,
+      selectedItemColor: theme.colorScheme.primary,
       unselectedItemColor: theme.colorScheme.onSurfaceVariant,
       selectedLabelStyle: theme.textTheme.bodyMedium,
       unselectedLabelStyle: theme.textTheme.bodySmall,
