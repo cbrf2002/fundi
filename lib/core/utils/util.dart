92d336cc8a1b8c2bbeb1594f4c2646962fc233b8
cbrf2002 <cbrf2002@proton.me>
2024-12-30 14:36:01 +0800

    util change

--- lib/core/utils/util.dart
+++ lib/core/utils/util.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class AppTextStyles {
-  static TextStyle get displayLarge => TextStyle(fontSize: 57, fontWeight: FontWeight.bold);
-  static TextStyle get displayMedium => TextStyle(fontSize: 45, fontWeight: FontWeight.bold);
-  static TextStyle get displaySmall => TextStyle(fontSize: 36, fontWeight: FontWeight.bold);
-  static TextStyle get headlineLarge => TextStyle(fontSize: 32, fontWeight: FontWeight.bold);
-  static TextStyle get headlineMedium => TextStyle(fontSize: 28, fontWeight: FontWeight.bold);
-  static TextStyle get headlineSmall => TextStyle(fontSize: 24, fontWeight: FontWeight.bold);
-  static TextStyle get titleLarge => TextStyle(fontSize: 22, fontWeight: FontWeight.bold);
-  static TextStyle get titleMedium => TextStyle(fontSize: 16, fontWeight: FontWeight.bold);
-  static TextStyle get titleSmall => TextStyle(fontSize: 14, fontWeight: FontWeight.bold);
-  static TextStyle get bodyLarge => TextStyle(fontSize: 16);
-  static TextStyle get bodyMedium => TextStyle(fontSize: 14);
-  static TextStyle get bodySmall => TextStyle(fontSize: 12);
-  static TextStyle get labelLarge => TextStyle(fontSize: 14, fontWeight: FontWeight.bold);
-  static TextStyle get labelMedium => TextStyle(fontSize: 12, fontWeight: FontWeight.bold);
-  static TextStyle get labelSmall => TextStyle(fontSize: 11, fontWeight: FontWeight.bold);
+TextTheme createTextTheme(
+    BuildContext context, String bodyFontString, String displayFontString) {
+  TextTheme baseTextTheme = Theme.of(context).textTheme;
+  TextTheme bodyTextTheme = GoogleFonts.getTextTheme(bodyFontString, baseTextTheme);
+  TextTheme displayTextTheme =
+      GoogleFonts.getTextTheme(displayFontString, baseTextTheme);
+  TextTheme textTheme = displayTextTheme.copyWith(
+    bodyLarge: bodyTextTheme.bodyLarge,
+    bodyMedium: bodyTextTheme.bodyMedium,
+    bodySmall: bodyTextTheme.bodySmall,
+    labelLarge: bodyTextTheme.labelLarge,
+    labelMedium: bodyTextTheme.labelMedium,
+    labelSmall: bodyTextTheme.labelSmall,
+  );
+  return textTheme;
 }
