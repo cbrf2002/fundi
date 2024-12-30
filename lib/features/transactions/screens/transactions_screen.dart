5e618d12f4e2800a6d4ad318d00ab1e8ee1a65d5
cbrf2002 <cbrf2002@proton.me>
2024-12-30 14:37:28 +0800

    refactoring

--- lib/features/transactions/screens/transactions_screen.dart
+++ lib/features/transactions/screens/transactions_screen.dart
@@ -1,24 +1,44 @@
 import 'package:flutter/material.dart';
-import '../../../core/services/firestore_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import '../../../core/models/transaction_model.dart' as model;
 import '../../../core/utils/util.dart';
 
-class TransactionsScreen extends StatelessWidget {
+class TransactionsScreen extends StatefulWidget {
   const TransactionsScreen({super.key});
 
   @override
+  _TransactionsScreenState createState() => _TransactionsScreenState();
+}
+
+class _TransactionsScreenState extends State<TransactionsScreen> {
+  late Future<void> _fetchData;
+  List<model.Transaction> transactions = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchData = _loadData();
+  }
+
+  Future<void> _loadData() async {
+    final querySnapshot = await FirebaseFirestore.instance.collection('transactions').get();
+    setState(() {
+      transactions = querySnapshot.docs.map((doc) => model.Transaction.fromMap(doc.data(), doc.id)).toList();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final FirestoreService firestoreService = FirestoreService();
     return DefaultTabController(
       length: 4,
       child: Scaffold(
         appBar: AppBar(
-          title: Text('Transactions', style: AppTextStyles.headlineSmall.copyWith(color: Theme.of(context).colorScheme.onPrimary)),
+          title: Text('Transactions', style: Theme.of(context).textTheme.headlineSmall),
           backgroundColor: Theme.of(context).colorScheme.primary,
           bottom: TabBar(
             labelColor: Theme.of(context).colorScheme.onPrimary, // Active tab text color
             unselectedLabelColor: Theme.of(context).colorScheme.onPrimary.withOpacity(0.6), // Inactive tab text color
-            tabs: [
+            tabs: const [
               Tab(text: 'Day'),
               Tab(text: 'Week'),
               Tab(text: 'Month'),
@@ -26,13 +46,27 @@ class TransactionsScreen extends StatelessWidget {
             ],
           ),
         ),
-        body: TabBarView(
-          children: [
-            TransactionsListView(timeFrame: 'day', firestoreService: firestoreService),
-            TransactionsListView(timeFrame: 'week', firestoreService: firestoreService),
-            TransactionsListView(timeFrame: 'month', firestoreService: firestoreService),
-            TransactionsListView(timeFrame: 'year', firestoreService: firestoreService),
-          ],
+        body: FutureBuilder(
+          future: _fetchData,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else {
+              return RefreshIndicator(
+                onRefresh: _loadData,
+                child: TabBarView(
+                  children: [
+                    TransactionsListView(timeFrame: 'day', transactions: transactions),
+                    TransactionsListView(timeFrame: 'week', transactions: transactions),
+                    TransactionsListView(timeFrame: 'month', transactions: transactions),
+                    TransactionsListView(timeFrame: 'year', transactions: transactions),
+                  ],
+                ),
+              );
+            }
+          },
         ),
       ),
     );
@@ -41,28 +75,19 @@ class TransactionsScreen extends StatelessWidget {
 
 class TransactionsListView extends StatelessWidget {
   final String timeFrame;
-  final FirestoreService firestoreService;
+  final List<model.Transaction> transactions;
 
-  const TransactionsListView({super.key, required this.timeFrame, required this.firestoreService});
+  const TransactionsListView({super.key, required this.timeFrame, required this.transactions});
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<List<model.Transaction>>(
-      stream: firestoreService.getTransactions(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return Center(child: CircularProgressIndicator());
-        }
-        var transactions = snapshot.data!;
-        return ListView.builder(
-          itemCount: transactions.length,
-          itemBuilder: (context, index) {
-            var transaction = transactions[index];
-            return ListTile(
-              title: Text(transaction.category, style: AppTextStyles.bodyMedium.copyWith(color: Theme.of(context).colorScheme.onBackground)),
-              subtitle: Text('Amount: ${transaction.amount}', style: AppTextStyles.bodySmall.copyWith(color: Theme.of(context).colorScheme.onSurface)),
-            );
-          },
+    return ListView.builder(
+      itemCount: transactions.length,
+      itemBuilder: (context, index) {
+        var transaction = transactions[index];
+        return ListTile(
+          title: Text(transaction.category, style: Theme.of(context).textTheme.bodyMedium),
+          subtitle: Text('Amount: ${transaction.amount}', style: Theme.of(context).textTheme.bodySmall),
         );
       },
     );
