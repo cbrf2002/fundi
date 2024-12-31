940867b4904c2f1e09964c87f565e44e66e560c3
cbrf2002 <cbrf2002@proton.me>
2024-12-31 11:40:09 +0800

    Refactored controller

---
+++ lib/features/auth/controllers/auth_controller.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import '../../../core/services/auth_service.dart';
+
+class AuthController {
+  final AuthService _authService = AuthService();
+
+  Future<User?> signInWithGoogle() async {
+    return await _authService.signInWithGoogle();
+  }
+
+  Future<User?> signInWithEmail(String email, String password) async {
+    return await _authService.signInWithEmail(email, password);
+  }
+
+  Future<User?> signUpWithEmail(String email, String password) async {
+    return await _authService.signUpWithEmail(email, password);
+  }
+
+  Future<void> signOut() async {
+    await _authService.signOut();
+  }
+}
