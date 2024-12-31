f5d7b4a98928184fce09f638bb943b9627356169
cbrf2002 <cbrf2002@proton.me>
2024-12-31 11:41:44 +0800

    Refactored widget

---
+++ lib/features/dashboard/widgets/dashboard_header.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class DashboardHeader extends StatelessWidget {
+  const DashboardHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final DateTime now = DateTime.now();
+    final String formattedDate = DateFormat('EEE, MMM d, yyyy').format(now);
+
+    return Padding(
+      padding: const EdgeInsets.only(top: 32.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('Dashboard', style: Theme.of(context).textTheme.displaySmall),
+          const SizedBox(height: 16),
+          Text(formattedDate, style: Theme.of(context).textTheme.titleSmall),
+          const SizedBox(height: 64),
+        ],
+      ),
+    );
+  }
+}
