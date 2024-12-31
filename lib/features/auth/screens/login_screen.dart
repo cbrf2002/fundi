e3656819b0de986d49842f70b0cb68583012928a
cbrf2002 <cbrf2002@proton.me>
2024-12-31 11:40:31 +0800

    Refactored for UI and widget separation

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:fundi/core/services/auth_service.dart';
-import '../widgets/email_sign_in_form.dart';
-import '../widgets/email_sign_up_form.dart';
+import '../widgets/login_background.dart';
+import '../widgets/login_header.dart';
+import '../widgets/login_form_container.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -13,9 +12,6 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  bool isSignUp = false;
-  bool isEmailExpanded = false;
-
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
@@ -33,141 +29,20 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
-  void toggleEmailExpansion() {
-    setState(() {
-      isEmailExpanded = !isEmailExpanded;
-      if (!isEmailExpanded) {
-        isSignUp = false; // Reset to sign-in when collapsing
-      }
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // Determine the logo based on the theme
-    final bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
-    final String logoPath = isDarkMode ? 'lib/assets/logo/logoDark.svg' : 'lib/assets/logo/logoLight.svg';
-
     return Scaffold(
       body: Stack(
         children: [
-          Positioned(
-            top: -100,
-            left: -100,
-            child: Image.asset(
-              'lib/assets/images/ell2.png',
-              width: 500,
-              height: 500,
-            ),
-          ),
-          Positioned(
-            bottom: -100,
-            right: -100,
-            child: Transform.rotate(
-              angle: 3.14159, // 180 degrees in radians
-              child: Image.asset(
-                'lib/assets/images/ell2.png',
-                width: 500,
-                height: 500,
-              ),
-            ),
-          ),
+          const LoginBackground(),
           Center(
             child: Padding(
               padding: const EdgeInsets.all(16.0),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SvgPicture.asset(
-                    logoPath,
-                    width: 200,
-                    height: 200,
-                  ),
-                  const SizedBox(height: 48),
-                  Text(
-                    'Easily track your expenses and make smart financial decisions with Fundi.',
-                    style: Theme.of(context).textTheme.titleMedium,
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(height: 32),
-                  AnimatedSize(
-                    duration: const Duration(milliseconds: 300),
-                    curve: Curves.easeInOut,
-                    child: Container(
-                      width: double.infinity,
-                      padding: const EdgeInsets.symmetric(horizontal: 16),
-                      child: isEmailExpanded
-                          ? Column(
-                              key: const ValueKey('ExpandedForm'),
-                              children: [
-                                AnimatedSwitcher(
-                                  duration: const Duration(milliseconds: 300),
-                                  child: isSignUp
-                                      ? EmailSignUpForm(key: const ValueKey('signUp'))
-                                      : EmailSignInForm(key: const ValueKey('signIn')),
-                                ),
-                                const SizedBox(height: 16),
-                                TextButton(
-                                  onPressed: () {
-                                    setState(() {
-                                      isSignUp = !isSignUp;
-                                    });
-                                  },
-                                  child: Text(isSignUp
-                                      ? 'Already have an account? Sign In'
-                                      : 'Don’t have an account? Sign Up'),
-                                ),
-                                const SizedBox(height: 16),
-                                TextButton(
-                                  onPressed: toggleEmailExpansion,
-                                  child: const Text('Back to Login Options'),
-                                ),
-                              ],
-                            )
-                          : Column(
-                              key: const ValueKey('CollapsedForm'),
-                              children: [
-                                OutlinedButton(
-                                  onPressed: toggleEmailExpansion,
-                                  style: OutlinedButton.styleFrom(
-                                    minimumSize: const Size.fromHeight(48),
-                                  ),
-                                  child: const Text('Sign In With Email'),
-                                ),
-                                const SizedBox(height: 16),
-                                FilledButton.icon(
-                                  onPressed: () async {
-                                    final user = await AuthService().signInWithGoogle();
-                                    if (user != null) {
-                                      ScaffoldMessenger.of(context).showSnackBar(
-                                        SnackBar(content: Text('Welcome, ${user.displayName}!')),
-                                      );
-                                      Navigator.pushNamed(context, '/dashboard');
-                                    }
-                                  },
-                                  icon: const Icon(Icons.g_translate),
-                                  label: const Text('Sign In With Google'),
-                                  style: FilledButton.styleFrom(
-                                    minimumSize: const Size.fromHeight(48),
-                                  ),
-                                ),
-                                const SizedBox(height: 16),
-                                TextButton(
-                                  onPressed: () {
-                                    setState(() {
-                                      isSignUp = true;
-                                      isEmailExpanded = true;
-                                    });
-                                  },
-                                  style: TextButton.styleFrom(
-                                    minimumSize: const Size.fromHeight(48),
-                                  ),
-                                  child: const Text('Create Account'),
-                                ),
-                              ],
-                            ),
-                    ),
-                  ),
+                children: const [
+                  LoginHeader(),
+                  LoginFormContainer(),
                 ],
               ),
             ),
